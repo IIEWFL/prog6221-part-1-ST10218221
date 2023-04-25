29d6b04e6e388a380443801b2288d117974ed0ed
ST10218221 <104359654+ST10218221@users.noreply.github.com>
2023-04-25 10:36:01 +0200

    Add files via upload
    Within this commit empty arrays were instantiated.

---
+++ ST10218221_JaidenNaioo_POEPart1_GR1/Recipe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10218221_JaidenNaioo_POEPart1_GR1
+{
+    internal class Recipe
+    {
+
+        private string[] ingredientNames;
+        private double[] ingredientQuantities;
+        private string[] ingredientUnits;
+        private string[] steps;
+        private double[] OriginalitQuantities;
+
+        public Recipe()
+        {
+            // Initialize empty arrays
+            ingredientNames = new string[0];
+            ingredientQuantities = new double[0];
+            ingredientUnits = new string[0];
+            steps = new string[0];
+        }
+
+    }
+}
