ff847c2102e4ef966a162ba56b97505f77714f89
ST10218221 <104359654+ST10218221@users.noreply.github.com>
2023-04-25 10:33:10 +0200

    Add files via upload
    Within this commit, a new class was created called Recipe with the sole purpose of housing all methods to be called in the main class. This commit also includes instantiation of arrays to be used.

---
+++ ST10218221_JaidenNaioo_POEPart1_GR1/ST10218221_JaidenNaioo_POEPart1_GR1/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10218221_JaidenNaioo_POEPart1_GR1
+{
+    internal class Recipe
+    {
+
+        private string[] ingredientNames;
+        private double[] ingredientQuantities;
+        private string[] ingredientUnits;
+        private string[] steps;
+        private double[] OriginalitQuantities;
+
+    }
+}
